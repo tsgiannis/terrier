c0db0fefbf46b5cb2765104dd3326abf9d2f04ec
tsgiannis <tsgiannis@gmail.com>
2023-04-21 20:24:48 +0300

    Working.....

--- batch_index.py
+++ batch_index.py
@@ -5,27 +5,40 @@ import pyterrier as pt
 if not pt.started():
   pt.init()
 # list of filenames to index
-files = pt.io.find_files("/home/masteripper/terrier-project-5.5/WPI_60K/data")
+
 #build the index
 indexer = pt.TRECCollectionIndexer("./wt2g_index", verbose=True, blocks=False)
-indexref = indexer.index(files)
+files = pt.io.find_files("/home/masteripper/terrier-project-5.5/WPI_60K/data")
+
 dest_index_path = "./wt2g_index"
-if not os.path.exists("{}/index.properties".format(dest_index_path)):
-  dataset = pt.get_dataset("irds:msmarco-passage")
-  iter_indexer = pt.IterDictIndexer(dest_index_path)
-  indexref = iter_indexer.index(dataset.get_corpus_iter(), meta=['docno', 'text'], meta_lengths=[20, 4096])
+if not os.path.exists(f"{dest_index_path}/data.properties"):
+  indexer = pt.TRECCollectionIndexer("./wt2g_index", verbose=True, blocks=False)
+  files = pt.io.find_files("/home/masteripper/terrier-project-5.5/WPI_60K/data")
+  indexref = indexer.index(files)
+
 else:
-  indexref = "{}/index.properties".format(dest_index_path)
+  indexref = f"{dest_index_path}/data.properties"
 
 # Create Pyterrier index object.
 index = pt.IndexFactory.of(indexref)
 
 
 
-topics_path = "/home/masteripper/terrier-project-5.5/WPI_60K/data/queries.txt"
+topics_path = "/home/masteripper/terrier-project-5.5/WPI_60K/queries.txt"
 topics= topics = pt.io.read_topics(topics_path, format='singleline')
 qrels_path ="/home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt"
 qrels = pt.io.read_qrels(qrels_path)
 # load the index, print the statistics
-index = pt.IndexFactory.of(indexref)
-print(index.getCollectionStatistics().toString())
+
+# Run Pyterrier experiment for BM25, DPH and DPH+BO1
+bm25 = pt.BatchRetrieve(index, wmodel="BM25", verbose=True)
+dph = pt.BatchRetrieve(index, wmodel='DPH', verbose=True)
+bo1 = dph >> pt.rewrite.Bo1QueryExpansion(index) >> dph
+
+pt.Experiment(
+    [bm25, dph, bo1],
+    topics,
+    qrels,
+    ["map"],
+    names=['bm25', 'dph', 'dph+bo1'],filter_by_topics=False,filter_by_qrels=False
+  )
