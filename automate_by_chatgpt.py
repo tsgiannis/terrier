42098da1ab7acd64668bbcc61bb1397616a03077
tsgiannis <tsgiannis@gmail.com>
2023-05-09 22:45:15 +0300

    Automation-partIV

--- automate_by_chatgpt.py
+++ automate_by_chatgpt.py
@@ -6,6 +6,23 @@ import random
 from random import sample
 import shutil
 
+def get_result_file(result_folder):
+    import os
+
+    folder_path = result_folder
+    extension = ".res"
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(extension):
+            return filename
+
+
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        # if the demo_folder directory is not present
+        # then create it.
+        os.makedirs(folder_path)
+
 #define Terrier
 terrier_exec = '/home/masteripper/terrier-project-5.5/bin/terrier'
 terrier_etc = '/home/masteripper/terrier-project-5.5/etc'
@@ -13,7 +30,7 @@ terrier_batch = '/home/masteripper/terrier-project-5.5/batch/index'
 terrier_batch_results = '/home/masteripper/terrier-project-5.5/batch/results'
 
 # Define the possible values for each property
-wmodels = random.choice(['BM25', 'DFR', 'DLH', 'DFRee','DPH'])
+wmodels = random.choice(['BM25', 'DFR',  'DFRee','InL2','Hiemstra_LM','PL2','BM25'])
 trecquerytags_processes = ['TOP','NUM','TITLE']
 trecquerytags_skips = ['DESC','NARR']
 termpipelines = ['Stopwords', 'PorterStemmer', 'Stopwords,PorterStemmer']
@@ -26,7 +43,7 @@ with open(terrier_properties) as f:
     lines = f.readlines()
 
 # Generate all possible combinations of the properties
-for i in range(20):
+for i in range(30):
     trecquerytags_processes_rnd = random.randint(1, len(trecquerytags_processes))
     trecquerytags_skips_rnd = random.randint(1, len(trecquerytags_skips))
     termpipelines_rnd = random.randint(1, len(termpipelines))
@@ -76,36 +93,50 @@ for i in range(20):
         else:
             new_lines.append(line)
     index_folder = os.path.join(terrier_batch,f"index_{i}")
-    result_folder = os.path.join(terrier_batch_results, f"index_{i}")
+    results_folder = os.path.join(terrier_batch_results, f"results_{i}")
     ##
     old_name = terrier_properties
     new_name = terrier_properties + f"_0{i}"
     os.rename(old_name, new_name)
-    new_lines.append(f'terrier.index.path={index_folder}/')
+    new_lines.append(f'terrier.index.path={index_folder}/\n')
+    new_lines.append(f'trec.results={results_folder}/\n')
+    new_lines.append("trec.topics = /home/masteripper/terrier-project-5.5/WPI_60K/queries.txt")
     with open(terrier_properties, 'w') as f:
         f.writelines(new_lines)
 #
-    if not os.path.exists(index_folder):
-        # if the demo_folder directory is not present
-        # then create it.
-        os.makedirs(index_folder)
+    # if not os.path.exists(index_folder):
+    #     # if the demo_folder directory is not present
+    #     # then create it.
+    #     os.makedirs(index_folder)
+        create_folder_if_not_exists(index_folder)
+        create_folder_if_not_exists(results_folder)
     #http://terrier.org/docs/v4.2/properties.html
 
         """  Indexing"""
     subprocess.run([f'{terrier_exec}', 'batchindexing'])
     """Retrieval"""
 #trec.results=/path/to/results/for/WT2G
-    subprocess.run([f'{terrier_exec}', 'batchindexing'])
+    subprocess.run([f'{terrier_exec}', 'batchretrieval'])
     """ Evaluating """
     #subprocess.run([f'{terrier_exec}', 'batchindexing'])
     # /trec_eval /home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt /home/masteripper/terrier-project-5.5/var_temp/results/DPH_0.res
     #https://stackoverflow.com/questions/4760215/running-shell-command-and-capturing-the-output
-    path_trec_eval = "home/masteripper/terrier-project-5.5/trec_eval/trec_eval"
-    results_file = ""
+    path_trec_eval = "/home/masteripper/terrier-project-5.5/bin/trec_eval"
+    results_file = get_result_file(results_folder)
+    results_file = os.path.join(results_folder,results_file)
     qrels_file = "/home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt"
+    #result = subprocess.run([f'{path_trec_eval}',f'{qrels_file}',f'{results_file}'],stdout=subprocess.PIPE)
     #result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
     terrier_properties_new_path = os.path.join(index_folder,"terrier.properties")
+    trec_eval_file = os.path.join(results_folder,"results.txt")
+    with open(trec_eval_file, "w") as output_file:
+        subprocess.run([f'{path_trec_eval}',f'{qrels_file}',f'{results_file}'], stdout=output_file)
+    trec_eval_score = os.path.join(results_folder, "results_score.txt")
+    with open(trec_eval_score, "w") as output_file:
+        subprocess.run([f'{path_trec_eval}','-m map',f'{qrels_file}',f'{results_file}'], stdout=output_file)
     shutil.copy(terrier_properties, terrier_properties_new_path)
+
+
     #subprocess.run(['terrier', 'batchretrieve', '-Dterrier.index.path=index_{}'.format(i), '-Dtrec.results.file=results_{}.txt'.format(i), '-p', 'terrier_{}.properties'.format(i)])
     #subprocess.run(['trec_eval', '-q', '-c', '-M1000', '-m', 'map', 'qrels.txt', 'results_{}.txt'.format(i)], stdout=open('eval_{}.txt'.format(i), 'w'))
 
