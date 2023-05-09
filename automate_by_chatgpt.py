fd6419a4de096a29b39d1f723c8545716fdb70f7
tsgiannis <tsgiannis@gmail.com>
2023-05-09 14:31:03 +0300

    Automation-partII

--- automate_by_chatgpt.py
+++ automate_by_chatgpt.py
@@ -2,23 +2,43 @@ import itertools
 import os
 import subprocess
 import os
+import random
+from random import sample
+import shutil
+
+#define Terrier
+terrier_exec = '/home/masteripper/terrier-project-5.5/bin/terrier'
+terrier_etc = '/home/masteripper/terrier-project-5.5/etc'
+terrier_batch = '/home/masteripper/terrier-project-5.5/batch/index'
+terrier_batch_results = '/home/masteripper/terrier-project-5.5/batch/results'
+
 # Define the possible values for each property
-wmodels = ['BM25', 'DFR', 'DLH', 'DFRee','DPH']
+wmodels = random.choice(['BM25', 'DFR', 'DLH', 'DFRee','DPH'])
 trecquerytags_processes = ['TOP','NUM','TITLE']
 trecquerytags_skips = ['DESC','NARR']
 termpipelines = ['Stopwords', 'PorterStemmer', 'Stopwords,PorterStemmer']
 fieldtags_processes = ['TITLE', 'ELSE']
-tokenizers = ['EnglishTokeniser', 'GalicianTokeniser']
+
 
 # Read the Terrier properties file
-with open('terrier.properties') as f:
+terrier_properties = os.path.join(terrier_etc,'terrier.properties')
+with open(terrier_properties) as f:
     lines = f.readlines()
 
 # Generate all possible combinations of the properties
-combinations = list(itertools.product(wmodels, trecquerytags_processes, trecquerytags_skips, termpipelines, fieldtags_processes, tokenizers))
+for i in range(20):
+    trecquerytags_processes_rnd = random.randint(1, len(trecquerytags_processes))
+    trecquerytags_skips_rnd = random.randint(1, len(trecquerytags_skips))
+    termpipelines_rnd = random.randint(1, len(termpipelines))
+    fieldtags_processes_rnd = random.randint(1, len(fieldtags_processes))
+    combinations = [wmodels,
+                    sample(trecquerytags_processes,trecquerytags_processes_rnd),
+                    sample(trecquerytags_skips,trecquerytags_skips_rnd),
+                    sample(termpipelines,termpipelines_rnd),
+                    sample(fieldtags_processes,fieldtags_processes_rnd)]
 
 # Generate a new properties file, execute indexing and retrieval for each combination
-for i, combination in enumerate(combinations):
+
     new_lines = []
     for line in lines:
         if line.startswith('querying.default.controls='):
@@ -27,7 +47,7 @@ for i, combination in enumerate(combinations):
             if len(result)>0 :
                 result_splitted = result[0].split('=')
                 result_resplitted =result_splitted[1].split(':')
-                result_resplitted[1] = combination[0]
+                result_resplitted[1] = combinations[0]
                 w_model  =':'.join(result_resplitted)
                 result_splitted[1] = w_model
                 quering_default = '='.join(result_splitted)
@@ -39,26 +59,52 @@ for i, combination in enumerate(combinations):
 
 
         elif line.startswith('TrecQueryTags.process='):
-            new_lines.append('TrecQueryTags.process={}\n'.format(combination[1]))
+            processed = ','.join(combinations[1])
+            new_lines.append(f'TrecQueryTags.process={processed}\n')
         elif line.startswith('TrecQueryTags.skip='):
-            new_lines.append('TrecQueryTags.skip={}\n'.format(combination[2]))
+            skipped = ','.join(combinations[2])
+            new_lines.append(f'TrecQueryTags.skip={skipped}\n')
         elif line.startswith('termpipelines='):
-            new_lines.append('termpipelines={}\n'.format(combination[3]))
+            term_lines = ','.join(combinations[3])
+            new_lines.append(f'termpipelines={term_lines}\n')
         elif line.startswith('FieldTags.process='):
-            new_lines.append('FieldTags.process={}\n'.format(combination[4]))
+            tag_process=  ','.join(combinations[4])
+            new_lines.append(f'FieldTags.process={tag_process}\n')
+        elif line.startswith('terrier.index.path'):
+            new_lines.append('# placeholder\n')
 
         else:
             new_lines.append(line)
-    index_folder = f'./index_{i}'
-    new_lines.append(f'terrier.index.path={index_folder }/')
-    with open('terrier_{}.properties'.format(i), 'w') as f:
+    index_folder = os.path.join(terrier_batch,f"index_{i}")
+    #
+    old_name = terrier_properties
+    new_name = terrier_properties + f"_0{i}"
+    os.rename(old_name, new_name)
+    new_lines.append(f'terrier.index.path={index_folder}/')
+    with open(terrier_properties, 'w') as f:
         f.writelines(new_lines)
 
-    if not os.path.exists(f"{index_folder }"):
+    if not os.path.exists(index_folder):
         # if the demo_folder directory is not present
         # then create it.
-        os.makedirs(f"{index_folder}")
-    subprocess.run(['terrier', 'indexing', '-Dterrier.index.path=index_{}'.format(i), '-p', 'terrier_{}.properties'.format(i)])
+        os.makedirs(index_folder)
+    #http://terrier.org/docs/v4.2/properties.html
+
+        """  Indexing"""
+    subprocess.run([f'{terrier_exec}', 'batchindexing'])
+    """Retrieval"""
+#trec.results=/path/to/results/for/WT2G
+    subprocess.run([f'{terrier_exec}', 'batchindexing'])
+    """ Evaluating """
+    #subprocess.run([f'{terrier_exec}', 'batchindexing'])
+    # /trec_eval /home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt /home/masteripper/terrier-project-5.5/var_temp/results/DPH_0.res
+    #https://stackoverflow.com/questions/4760215/running-shell-command-and-capturing-the-output
+    path_trec_eval = "home/masteripper/terrier-project-5.5/trec_eval/trec_eval"
+    results_file = ""
+    qrels_file = "/home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt"
+    #result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
+    terrier_properties_new_path = os.path.join(index_folder,"terrier.properties")
+    shutil.copy(terrier_properties, terrier_properties_new_path)
     #subprocess.run(['terrier', 'batchretrieve', '-Dterrier.index.path=index_{}'.format(i), '-Dtrec.results.file=results_{}.txt'.format(i), '-p', 'terrier_{}.properties'.format(i)])
     #subprocess.run(['trec_eval', '-q', '-c', '-M1000', '-m', 'map', 'qrels.txt', 'results_{}.txt'.format(i)], stdout=open('eval_{}.txt'.format(i), 'w'))
 
--- batch_index.py
+++ batch_index.py
@@ -29,16 +29,18 @@ topics= topics = pt.io.read_topics(topics_path, format='singleline')
 qrels_path ="/home/masteripper/terrier-project-5.5/WPI_60K/50_topics_eval_students/eval_qrels.txt"
 qrels = pt.io.read_qrels(qrels_path)
 # load the index, print the statistics
-
+BM25_br = pt.BatchRetrieve(index, wmodel="BM25")
+res = BM25_br.transform(topics)
+pt.Utils.evaluate(res, qrels, metrics = ['map'])
 # Run Pyterrier experiment for BM25, DPH and DPH+BO1
-bm25 = pt.BatchRetrieve(index, wmodel="BM25", verbose=True)
-dph = pt.BatchRetrieve(index, wmodel='DPH', verbose=True)
-bo1 = dph >> pt.rewrite.Bo1QueryExpansion(index) >> dph
-
-pt.Experiment(
-    [bm25, dph, bo1],
-    topics,
-    qrels,
-    ["map"],
-    names=['bm25', 'dph', 'dph+bo1'],filter_by_topics=False,filter_by_qrels=False
-  )
+# bm25 = pt.BatchRetrieve(index, wmodel="BM25", verbose=True)
+# dph = pt.BatchRetrieve(index, wmodel='DPH', verbose=True)
+# bo1 = dph >> pt.rewrite.Bo1QueryExpansion(index) >> dph
+#
+# pt.Experiment(
+#     [bm25, dph, bo1],
+#     topics,
+#     qrels,
+#     eval_metrics=["map", "recip_rank"],
+#     names=['bm25', 'dph', 'dph+bo1'],filter_by_topics=False,filter_by_qrels=False
+#   )
